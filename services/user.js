747279f599a7ca52ae0770b1973294a93d1bf55b
Alexander Marquez <alex18escorpio@gmail.com>
2022-06-29 09:01:00 -0500

    prueba

--- services/user.js
+++ services/user.js
@@ -2,14 +2,16 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 const loginService = async ({ email, password }) => {
-  const user = await User.findOne({ email }).select("password").exec();
+  console.log("llego a servicio", email, password);
+  /*
+  const user = await User.findOne({ email }).select("password",).exec();
   const type = await User.findOne({ email }).select("tipe").exec();
   if (!user) {
     return { message: "enter valid data", code: 200 };
   }
   return (await bcrypt.compare(password, user.password))
     ? { message: user.getToken(), code: 200, type: type.tipe }
-    : { message: "enter valid data", code: 400 };
+    : { message: "enter valid data", code: 400 };*/
 };
 
 const registerService = ({ email, password }) =>
